176dd4f6ab00fa201aeb7b8a15488076b166577f
Paulolivo4 <130105771+Paulolivo4@users.noreply.github.com>
2023-07-03 12:39:04 -0500

    Add files via upload

---
+++ practica4-conversor.c
@@ -0,0 +1,10 @@
+ #include <stdio.h>
+ #include <stdlib.h>
+
+ int main()
+ {
+float euros;
+printf("Introduce los euros a convertir\n");
+scanf("%f",&euros);
+printf("%.2f dolares equivale a %.2f pesetas",euros,euros*166.386);
+ }
